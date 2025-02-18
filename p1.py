b2bb3b543b8bb47c323fb3d0c899305a2da1ef44
rishikajr76 <rishikajr446@gmail.com>
2025-02-18 10:35:08 +0530

    new

--- p1.py
+++ p1.py
@@ -1,3 +1,8 @@
-i=-1
-i=-1
-print('i=',i)
+import math
+n = int(input("enter number:"))
+root = math.sqrt(n)
+root1 = int(root)
+if root1 * root1 == n:
+    print("Perfect square")
+else:
+    print("Not Perfect square")    
