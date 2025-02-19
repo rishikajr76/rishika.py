b88627a0e25e0ddfcf817bf9265429a85f0b36f3
rishikajr76 <rishikajr446@gmail.com>
2025-02-19 15:53:25 +0530

    new

---
+++ db2.py
@@ -0,0 +1,25 @@
+import pymysql
+
+def connect_db():
+    try:
+        connection = pymysql.Connect(host='localhost', 
+                                 port=3306, user='root', 
+                                 password='root', 
+                                 database='rishika_db', 
+                                 charset='utf8')
+        print('Database connected')
+        return connection
+    except:
+        print('Database connection failed')
+            
+    return connection
+
+def disconnect_db(connection):
+    try:
+        connection.close()
+        print('Database disconnected')
+    except:
+        print('Error while disconnecting Database')   
+
+connection = connect_db()
+disconnect_db(connection)
