bc2e9017afbfbf855ef9e30ab4b878ca8e655203
rishikajr76 <rishikajr446@gmail.com>
2025-02-20 15:15:40 +0530

    done

--- sum.py
+++ sum.py
@@ -1,6 +1,6 @@
-num=int(input("Enter a number:"))
-s1=str(num)
-sum1=0
+num = int(input("Enter a number:"))
+s1 = str(num)
+sum1 = 0
 for i in range(len(s1)):
     if i % 2 != 0:
         if int(s1[i]) % 2 == 0:
