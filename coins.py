0f4eecf17db73099d995aa5ca316b3dca1c4ec96
rishikajr76 <rishikajr446@gmail.com>
2025-02-21 16:23:06 +0530

    distribution of coins

---
+++ coins.py
@@ -0,0 +1,15 @@
+def get_coins(amount):
+    coins = [5, 2, 1]
+    count = {}
+    for coin in coins:
+        count[coin] = amount // coin
+        amount %= coin
+    return count
+
+
+amount = int(input("Enter the amount of money:"))
+count = get_coins(amount)
+
+print("Coin distribution:")
+for coin, count in count.items():
+    print(f"{coin} rupee coin: {count}")
